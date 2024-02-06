da5c17f38eccc1b3ffe52d6b1c717a1ccf5f1e62
limesoda314 <limesoda314@gmail.com>
2024-02-05 21:12:01 -0800

    Added the _redirects

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import "./src/styles/global.css"
 // gatsby-browser.js
 import React from 'react';
-import Layout from './src/pages/Layout';
+import Layout from "./src/pages/Layout";
 
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
